9c53ca779a065581f51d9f13760d489efb9393a0
Eugene Khashin <adastreamer@gmail.com>
2016-04-10 20:58:52 -0700

    gulp build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,20 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('default', function() {
   gutil.log('Nothing to do here!');
+});
+
+gulp.task('sass', function () {
+  return gulp.src('./scss/index.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./bin'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./scss/**/*.scss', ['sass']);
 });
